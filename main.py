21575f5650799d3c78be8a567824b741eab5d9b9
Lilia <lilia.mehamli@protonmail.com>
2022-01-17 19:58:33 +0000

    add basic gui layout

--- main.py
+++ main.py
@@ -1,119 +1,78 @@
-import threading
-from time import sleep
-
-CLOCK_TOP = 1
-LATENCY = 6
-SUM_PRIORITIES = 14138
-KILO = 1024
-DICT_PROCESSES = {
-    # "COLOR": [PRIORITY, T_CPU, QUANTUM, EXECUTION_VALUE]
-    "WHITE": [9548, 0, 0, 0],
-    "RED": [3121, 0, 0, 0],
-    "GREEN": [1024, 0, 0, 0],
-    "BLUE": [335, 0, 0, 0],
-    "YELLOW": [110, 0, 0, 0],
-}
-CURRENT_PROCESS = ""
+# Imports each and every method and class
+# of module tkinter and tkinter.ttk
+from tkinter import *
+from utils import fillQuantums, DICT_PROCESSES
 
 
-def getTOP():
-    print("Current Process: ", CURRENT_PROCESS)
+def create_circles_frame(container):
+    # Creates a object of class canvas
+    frame = Canvas(container,  height="130")
+    # Creates a circle of diameter 80
+    (y0, y1) = (20, 130)
+    (x0, x1) = (20, 130)
+    for process in DICT_PROCESSES:
+        frame.create_oval(x0, y0, x1, y1, outline="black", fill=process,
+                          width=0)
+        x0 = x1 + 40
+        x1 = x0 + (y1-y0)
+
+    # Pack the canvas to the main window and make it expandable
+    frame.pack(fill=BOTH)
+
+    return frame
+
+
+def create_labels_frame(container):
+    frame = Frame(container)
+    # Add labels
     for process in DICT_PROCESSES:
         array = DICT_PROCESSES[process]
-        print(f"-{process}: CPU: {array[1]} \t\t EXEC_VALUE: {array[3]}")
-        print("\n")
+        Label(frame, text=str(round(array[2]*1000))+"ms", width=15,
+              padx=15, pady=20).pack(side=LEFT)
+
+    return frame
 
 
-def appendNewProcess():
-    global CURRENT_PROCESS
-    # Initialize value of min_exec_value
-    process = list(DICT_PROCESSES.keys())[0]
-    min_exec_value = DICT_PROCESSES[process][3]
+def create_buttons_frame(container):
+    frame = Frame(container)
+    # Add ok button
+    ok_button = Button(frame, text='Demarrer', width="30", height="2")
+    ok_button.pack(side=TOP, padx=5, pady=5)
+    # Add cancel button
+    cancel_button = Button(frame, text='Arreter', width="30", height="2")
+    cancel_button.pack(side=BOTTOM, padx=5)
 
-    for process in DICT_PROCESSES:
-        # Get array of data
-        array = DICT_PROCESSES[process]
-        exec_value = array[3]
-        if exec_value == 0:
-            CURRENT_PROCESS = process
-            break
-        else:
-            if(exec_value <= min_exec_value):
-                min_exec_value = exec_value
-                CURRENT_PROCESS = process
+    return frame
 
 
-def calculateT_CPU(quantum):
-    # Get array of data
-    array = DICT_PROCESSES[CURRENT_PROCESS]
-    # Replace old T_CPU
-    array[1] = array[1] + quantum
-    # Replace array of data
-    DICT_PROCESSES[CURRENT_PROCESS] = array
+class Shape:
+    def __init__(self, master=None):
+        self.master = master
+        # Calls create method of class Shape
+        self.create()
 
+    def create(self):
+        circles_frame = create_circles_frame(self.master)
+        circles_frame.pack(anchor=NW)
 
-class calculateExecValue(object):
-    def __init__(self, interval=CLOCK_TOP):
-        self.interval = interval
+        labels_frame = create_labels_frame(self.master)
+        labels_frame.pack(anchor=NW)
 
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True
-        thread.start()
-
-    def run(self):
-        while True:
-            #  Execution value of a process (i) = time spent in CPU * 1024/ priority of process (i).
-            for process in DICT_PROCESSES:
-                # Get array of data
-                array = DICT_PROCESSES[process]
-                # Calculate new EXEC_VALUE
-                exec_value = array[1]*KILO/array[0]
-                # Replace old EXEC_VALUE
-                array[3] = exec_value
-                # Replace array of data
-                DICT_PROCESSES[process] = array
-            sleep(self.interval)
-
-
-def getDataByColor(color):
-    # Fetch array of data from dictionary by color
-    array = DICT_PROCESSES[color]
-    return array
-
-
-def fetchCurrentQuantum():
-    return DICT_PROCESSES[CURRENT_PROCESS][2]
-
-
-def fillQuantums():
-    # Quantum of process (i) = latency * priority of process (i) / sum of priorities
-    for process in DICT_PROCESSES:
-        # Get array of data
-        array = DICT_PROCESSES[process]
-        # Calculate new EXEC_VALUE
-        quantum = LATENCY*array[0]/SUM_PRIORITIES
-        # Replace old EXEC_VALUE
-        array[2] = quantum
-        # Replace array of data
-        DICT_PROCESSES[process] = array
-
-
-def main():
-    # Fill dictionary with the value of quantum respective to each process
-    fillQuantums()
-    # Update EXEC_VALUE every clock top
-    thread_exec_value = calculateExecValue()
-    while True:
-        # Append process to memory
-        appendNewProcess()
-        # Fetch quantum of current process
-        quantum = fetchCurrentQuantum()
-        sleep(quantum)
-        # Update T_CPU
-        calculateT_CPU(quantum)
-        # Get state of processes
-        getTOP()
+        buttons_frame = create_buttons_frame(self.master)
+        buttons_frame.pack(anchor=SE)
 
 
 if __name__ == "__main__":
-    main()
+    # Fill quantums
+    fillQuantums()
+    # object of class Tk, responsible for creating
+    # a tkinter toplevel window
+    master = Tk()
+    shape = Shape(master)
+    # Sets the title to Shapes
+    master.title("Simulation d'algorithme de gestion de processus")
+    # Sets the geometry and position of window on the screen
+    master.geometry("750x300")
+    master.columnconfigure(1, weight=1)
+    # Infinite loop breaks only by interrupt
+    master.mainloop()
